2e17cdae36a5a659493ecfc4d61507680bc739af
Denis <pdef8085@gmail.com>
2020-08-20 23:08:54 +0300

    Add reverse feature

--- ViceVers/ViceVers/views.py
+++ ViceVers/ViceVers/views.py
@@ -7,4 +7,6 @@ def home(request):
 
 
 def reverse(request):
-    return render(request, 'reverse.html')
+    user_text = request.GET['usertext']
+    reverse_text = user_text[::-1]
+    return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reverse_text})
