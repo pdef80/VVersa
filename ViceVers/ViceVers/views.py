d810d844ae5444644db89f5045abcf8a7396af7d
Denis <pdef8085@gmail.com>
2020-08-20 23:16:27 +0300

    Add number of words

--- ViceVers/ViceVers/views.py
+++ ViceVers/ViceVers/views.py
@@ -9,4 +9,5 @@ def home(request):
 def reverse(request):
     user_text = request.GET['usertext']
     reverse_text = user_text[::-1]
-    return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reverse_text})
+    number = len(user_text.split(' '))
+    return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reverse_text, 'number': number})
